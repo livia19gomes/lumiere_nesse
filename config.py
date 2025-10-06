6c2460805ca7ea2171e83009abc6c18e0696ea77
livia19gomes <167575522+livia19gomes@users.noreply.github.com>
2025-10-06 17:26:36 -0300

    ALTERAÇÕES LAIS

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ SECRET_KEY = 'chave_secreta'
 UPLOAD_FILMES = os.path.join(os.getcwd(), 'static/uploads')
 DEBUG = True
 DB_HOST = 'localhost'
-DB_NAME = r'C:\Users\Aluno\Downloads\LUMIERE.FDB'
+DB_NAME = r'C:\Users\Aluno\Desktop\Nova pasta\lumiere_nesse\BANCO.FDB'
 
 
 DB_USER = 'sysdba'
